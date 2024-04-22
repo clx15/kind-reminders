cc9850710b4f86d92650e9396c594fc472e41ae8
Laur <wtfomgdlc@gmail.comgit config --global user.name Laurgit commitgit config --global user.email wtfomgdlc@gmail.com>
2024-04-22 17:45:50 +0300

    salut

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "./notes";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+      <div>
+        <Header />
+        {notes.map(notes =>
+        <Note
+          key = {notes.id}
+          title = {notes.title}
+          content = {notes.content}
+        />)}
+        <Footer />
+      </div>
+   
   );
 }
 
